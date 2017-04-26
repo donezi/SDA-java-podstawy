b6726e6e46182bc1cc4bc02e6bbd9236818373b0
unknown <jakub.piechocki@gmail.com>
2017-04-26 18:29:50 +0200

    Using switch

--- src/zajecia/oop/quiz/Quiz.java
+++ src/zajecia/oop/quiz/Quiz.java
@@ -17,31 +17,38 @@ public class Quiz {
         while (gameFlag) {
             //while (decision !=0 )
             int decision = quizInterface.menu();
-            if (decision == 1) {
-                String playerName = quizInterface.insertName();
-                quizInterface.beforeStart();
-                
-                Question[] questions = questionsRepository.getQuestions();
-                int correctAnswersCounter = 0;
-                for (int i = 0; i < questions.length; i++) {
-                    boolean result = quizInterface.showQuestion(questions[i]);
-                    if (result) {
-                        quizInterface.correctAnswer();
-                        correctAnswersCounter++;
-                    } else {
-                        quizInterface.incorrectAnswer();
+            switch (decision) {
+                case 1:
+                    String playerName = quizInterface.insertName();
+                    quizInterface.beforeStart();
+
+                    Question[] questions = questionsRepository.getQuestions();
+                    int correctAnswersCounter = 0;
+                    for (int i = 0; i < questions.length; i++) {
+                        boolean result = quizInterface.showQuestion(questions[i]);
+                        if (result) {
+                            quizInterface.correctAnswer();
+                            correctAnswersCounter++;
+                        } else {
+                            quizInterface.incorrectAnswer();
+                        }
                     }
-                }
-                quizInterface.showResult(playerName, correctAnswersCounter);
-            } else if (decision == 2) {
-                quizInterface.showTopResult(mockResultRepository.getTopResults(10));
-            } else {
-                gameFlag = false;
+                    quizInterface.showResult(playerName, correctAnswersCounter);
+                    break;
+                case 2:
+                    quizInterface.showTopResult(mockResultRepository.getTopResults(10));
+                    break;
+                case 3:
+                    gameFlag = false;
+                    break;
+                default:
+                    System.out.println("Wrong decision");
             }
         }
         quizInterface.afterGameEnded();
     }
 }
+
 //        Question[] questions = questionsRepository.getQuestions();
 //        for (int i = 0; i < questions.length; i++) {
 //            System.out.println(questions[i]);
